a49e7a1b7e7ab6d6d65d290e1b855b08d0ed68fc
Tyler Southwick <tyler.southwick@nike.com>
2016-09-14 09:15:17 -0700

    update to newer redwiggler and BuildInfo

--- src/main/scala/com/nike/sbt/plugins/redwiggler/SbtRedWiggler.scala
+++ src/main/scala/com/nike/sbt/plugins/redwiggler/SbtRedWiggler.scala
@@ -2,25 +2,34 @@ package com.nike.sbt.plugins.redwiggler
 
 import sbt._
 import Keys._
-import com.nike.retail.redwiggler.RedWiggler
+import com.nike.redwiggler.common.{ResultValidator, Configuration => RedWigglerConfiguration}
 
 object SbtRedWiggler extends sbt.AutoPlugin {
 
   object autoImport {
     val blueprintFile = settingKey[File]("blueprint API.md file")
+    val swaggerFile = settingKey[File]("swagger.yaml file")
     val dataDirectory = settingKey[File]("setting directory")
 
     val redwiggler = taskKey[File]("Runs RedWiggler")
   }
 
+  val redwigglerRestAssured = "com.nike.redwiggler" % "redwiggler-restassured" % BuildInfo.redwigglerVersion
+
   import autoImport._
 
   override def projectSettings: Seq[Def.Setting[_]] = Seq(
     blueprintFile := new File(baseDirectory.value, "API.md"),
+    swaggerFile := new File(baseDirectory.value, "swagger.yamld"),
     dataDirectory := new File(target.value, "redwiggler-input"),
     target in redwiggler := new File((target in Compile).value, "redwiggler.html"),
-    redwiggler <<= (target in redwiggler, dataDirectory, blueprintFile) map { (output, dataDir, mdFile) =>
-      RedWiggler.validateResultsAgainstMarkdown(mdFile, dataDir, output)
+    redwiggler <<= (target in redwiggler, dataDirectory, blueprintFile, swaggerFile) map { (output, dataDir, mdFile, swagger) =>
+      ResultValidator.validateResults(new RedWigglerConfiguration {
+          val getMarkdownFile = mdFile
+          val getSwaggerFile = swagger
+          val getDataDirectory = dataDir
+          val getOutput = output
+      })
       output
     }
   )
